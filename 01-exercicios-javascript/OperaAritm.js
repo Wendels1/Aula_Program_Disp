5df22be7afb3fa43483a50e4e7f41e9a0737b3dd
Wendels1 <wende.ferreira@iesb.edu.br>
2025-03-06 21:50:47 -0300

    Exercício 2: Operadores Aritméticos e de Comparação

---
+++ 01-exercicios-javascript/OperaAritm.js
@@ -0,0 +1,8 @@
+const num1 = 15;
+const num2 = 5;
+
+console.log("Soma:", num1 + num2);
+console.log("Subtração:", num1 - num2);
+console.log("Multiplicação:", num1 * num2);
+console.log("Divisão:", num1 / num2);
+console.log("Primeiro é maior?", num1 > num2);
