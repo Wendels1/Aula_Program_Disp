f149b076a666f27fc255602455f461c6dfb89a87
Wendels1 <wende.ferreira@iesb.edu.br>
2025-03-06 22:14:03 -0300

    Consertando a pasta

--- 01-exercicios-javascript/ManiString.js
+++
@@ -1,8 +0,0 @@
-const string = "Wendel Ferreira Santos";
-const letraAntiga = 'l';
-const letraNova = 'z';
-
-console.log("Maiúsculas:", string.toUpperCase());
-console.log("Minúsculas:", string.toLowerCase());
-console.log("Invertida:", string.split('').reverse().join(''));
-console.log("Substituída:", string.replace(new RegExp(letraAntiga, 'g'), letraNova));
--- 01-exercicios-javascript/OperaAritm.js
+++
@@ -1,8 +0,0 @@
-const num1 = 15;
-const num2 = 5;
-
-console.log("Soma:", num1 + num2);
-console.log("Subtração:", num1 - num2);
-console.log("Multiplicação:", num1 * num2);
-console.log("Divisão:", num1 / num2);
-console.log("Primeiro é maior?", num1 > num2);
