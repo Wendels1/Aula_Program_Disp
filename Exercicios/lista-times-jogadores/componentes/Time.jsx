2db92999d302c596ad28391d52284c172cae111d
Wendels1 <wendel.ferreira@iesb.edu.br>
2025-04-03 22:01:21 -0300

    Exercicio lista times jogadores

---
+++ Exercicios/lista-times-jogadores/componentes/Time.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text } from 'react-native'
+import {  Card } from 'react-native-paper'
+import Jogador from './Jogador'
+
+
+export default function Time(props) {
+
+     const { nome, anoFundacao, mascote, imagem, jogadores } = props
+     console.log(props)
+
+  return (
+   
+    <Card style={{margin: 10}}>
+        <Card.Title title={nome} subtitle={anoFundacao} />
+        <Card.Content>
+            <Text>Mascote: {mascote}</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: imagem }} />
+        <Card.Actions>
+            <FlatList
+                horizontal
+                data={jogadores}
+                renderItem={({ item }) => (
+                    <Jogador
+                        nome={item.nome}
+                        numero={item.numero}
+                        imagem={item.imagem}
+                    />
+                )}
+            />
+        </Card.Actions>
+        
+
+    </Card>
+    
+      
+    
+  )
+}
+
+const styles = StyleSheet.create({})
