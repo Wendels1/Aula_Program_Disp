43603072739719e16198427405614d733b1453b0
Wendels1 <wendel.ferreira@iesb.edu.br>
2025-04-09 21:10:18 -0300

    Aula 08-Navegacao-abas

---
+++ 08-Navegacao-abas/screens/ProfileScreen.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>ProfileScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+})
