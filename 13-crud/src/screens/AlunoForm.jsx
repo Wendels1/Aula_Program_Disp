c4ff1f269ed7b102351fc364427b7a6fda9e43e8
Wendels1 <wendel.ferreira@iesb.edu.br>
2025-05-29 21:47:32 -0300

    14 - Aula 14: Async Storage

--- 13-crud/src/screens/AlunoForm.jsx
+++ 13-crud/src/screens/AlunoForm.jsx
@@ -2,134 +2,154 @@ import { StyleSheet, View } from 'react-native'
 import React, { useState } from 'react'
 import { Text, Button, TextInput } from 'react-native-paper'
 import { TextInputMask } from 'react-native-masked-text'
+import AlunoService from '../alunos/AlunoService'
 
 export default function AlunoForm({ navigation, route }) {
 
-    const [nome, setNome] = useState('')
-    const [cpf, setCpf] = useState('')
-    const [email, setEmail] = useState('')
-    const [telefone, setTelefone] = useState('')
-    const [dataNascimento, setDataNascimento] = useState('')
-
-    function salvar() {
-        const aluno = {
-          nome,
-          cpf,
-          email,
-          telefone,
-          dataNascimento
-        }
-    
-        if (!aluno.nome || !aluno.cpf || !aluno.email || !aluno.telefone || !aluno.dataNascimento) {
-          alert('Preencha todos os campos!!!')
-        } else {
-          alert(JSON.stringify(aluno))
-        }
-    
+    const alunoAntigo = route.params || {}
+  
+    const [nome, setNome] = useState(alunoAntigo.nome || '')
+    const [cpf, setCpf] = useState(alunoAntigo.cpf || '')
+    const [email, setEmail] = useState(alunoAntigo.email || '')
+    const [telefone, setTelefone] = useState(alunoAntigo.telefone || '')
+    const [dataNascimento, setDataNascimento] = useState(alunoAntigo.dataNascimento || '')
+  
+  
+    async function salvar() {
+      const aluno = {
+        nome,
+        cpf,
+        email,
+        telefone,
+        dataNascimento
       }
-
-  return (
-    <View>
-      <Text variant='headlineLarge' style={{ textAlign: 'center', margin: 10 }}>Informe os Dados</Text>
-
+  
+      if (!aluno.nome || !aluno.cpf || !aluno.email || !aluno.telefone || !aluno.dataNascimento) {
+        alert('Preencha todos os campos!!!')
+        return
+      }
+  
+  
+      if (alunoAntigo.id) {
+        // Atualizar um aluno
+        aluno.id = alunoAntigo.id
+        await AlunoService.atualizar(aluno)
+        alert('Aluno atualizado com sucesso!')
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "AlunoLista" }]
+        })
+      } else {
+        // criando um aluno novo
+        await AlunoService.salvar(aluno)
+        alert('Aluno criado com sucesso!')
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "AlunoLista" }]
+        })
+      }
+  
+  
+    }
+  
+    return (
+      <View style={styles.container}>
+        <Text variant='titleLarge' style={{ marginTop: 10 }}>Informe os dados:</Text>
+        <Text variant='titleLarge' style={{ marginTop: 10 }}>Aluno ID: {alunoAntigo.id || 'NOVO'}</Text>
+  
         <TextInput
-            label='Nome'
-            mode='outlined'
-            style={styles.input}
-            placeholder='Informe o Nome'
-            value={nome}
-            onChange={setNome}
+          label='Nome'
+          style={styles.input}
+          mode='outlined'
+          value={nome}
+          onChangeText={setNome}
         />
-
+  
         <TextInput
-            label='CPF'
-            mode='outlined'
-            style={styles.input}
-            placeholder='Informe o CPF'
-            value={cpf}
-            onChange={setNome}
-            keyboardType='numeric'
-            render={(props) => (
-                <TextInputMask
-                    {...props}
-                    type={'cpf'}
-                    options={{
-                        format: '999.999.999-99'
-                    }}
-                />
-            )}
+          label='CPF'
+          style={styles.input}
+          mode='outlined'
+          value={cpf}
+          onChangeText={setCpf}
+          keyboardType='numeric'
+          render={(props) => (
+            <TextInputMask
+              {...props}
+              type={'cpf'}
+            />
+          )}
         />
-
+  
         <TextInput
-            label='E-mail'
-            style={styles.input}
-            mode='outlined'
-            value={email}
-            onChangeText={setEmail}
-            keyboardType='email-address'
+          label='E-mail'
+          style={styles.input}
+          mode='outlined'
+          value={email}
+          onChangeText={setEmail}
+          keyboardType='email-address'
         />
-        
+  
         <TextInput
-            label='Telefone'
-            mode='outlined'
-            style={styles.input}
-            placeholder='Informe o Telefone'
-            value={telefone}
-            onChange={setNome}
-            render={(props) => (
-                <TextInputMask
-                    {...props}
-                    type={'cel-phone'}
-                    options={{
-                        maskType: 'BRL',
-                        withDDD: true,
-                        dddMask: '(99) 9999-9999 '
-                    }}
-                />
-            )}
+          label='Telefone'
+          style={styles.input}
+          mode='outlined'
+          value={telefone}
+          onChangeText={setTelefone}
+          keyboardType='numeric'
+          placeholder='(61)90000-0001'
+          render={(props) => (
+            <TextInputMask
+              {...props}
+              type={'cel-phone'}
+              options={{
+                maskType: 'BRL',
+                withDDD: true,
+                dddMask: '(99) '
+              }}
+            />
+          )}
         />
-
+  
         <TextInput
-            label='Data de Nascimento'
-            mode='outlined'
-            style={styles.input}
-            placeholder='Informe a Data de Nascimento'
-            value={dataNascimento}
-            onChange={setNome}
-            keyboardType='numeric'
-            render={(props) => (
-                <TextInputMask
-                    {...props}
-                    type={'datetime'}
-                    options={{
-                        mask: 'DD/MM/YYYY'
-                    }}
-                />
-            )}
+          label='Data de Nascimento'
+          style={styles.input}
+          mode='outlined'
+          value={dataNascimento}
+          onChangeText={setDataNascimento}
+          keyboardType='numeric'
+          placeholder='01/01/2000'
+          render={(props) => (
+            <TextInputMask
+              {...props}
+              type={'datetime'}
+              options={{
+                format: 'DD/MM/YYYY'
+              }}
+            />
+          )}
         />
-
-
+  
         <Button
-            mode='contained'
-            icon='content-save'
-            style={{ margin: 10 }}
-            onPress={salvar}
+          style={styles.input}
+          mode='contained'
+          icon='plus'
+          onPress={salvar}
         >
-            Salvar
+          Salvar
         </Button>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
+  
+  
+      </View>
+    )
+  }
+  
+  const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 10,
-        alignItems: 'center',
+      flex: 1,
+      alignItems: 'center'
     },
     input: {
-        marginBottom: 10,
-        width: '90%',
-        marginTop: 10,
+      width: '90%',
+      marginTop: 10
     }
-})
+  })
--- 13-crud/src/screens/AlunoLista.jsx
+++ 13-crud/src/screens/AlunoLista.jsx
@@ -2,43 +2,33 @@
 import { Card, Text, Button } from 'react-native-paper'
 import { View, FlatList } from 'react-native'
 import React, { useState, useEffect }from 'react'
+import AlunoService from '../alunos/AlunoService'
 
+export default function AlunoLista({ navigation, route }) {
 
-export default function AlunoLista({navigation}) {
+  const [alunos, setAlunos] = useState([])
 
-    const [alunos, setAlunos] = useState([
-        {
-            id: 1,
-            nome: 'João',
-            cpf: '12345678901',
-            email: 'Joazin@gmai.com',
-            telefone: '(61) 99999-9999',
-            dataNascimento: '01/01/2000',
-        },
-        {
-            id: 2,
-            nome: 'Maria',
-            cpf: '98765432100',
-            email: 'Maria@gmail.com',
-            telefone: '(61) 88888-8888',
-            dataNascimento: '02/02/2001',
-        },
-        {
-            id: 3,
-            nome: 'José',
-            cpf: '12345678902',
-            email: 'José@gmail.com',
-            telefone: '(61) 77777-7777',
-            dataNascimento: '03/03/2002',
-        }
-    ])
+  useEffect(() => {
+    buscarAlunos()
+  }, [])
+
+  async function buscarAlunos() {
+    const listaAlunos = await AlunoService.listar()
+    setAlunos(listaAlunos)
+  }
+
+  async function excluirAluno(id) {
+    await AlunoService.remover(id)
+    buscarAlunos()
+    alert('Aluno excluido com sucesso!')
+  }
 
   return (
     <View>
       <Button
+        style={{ margin: 10 }}
         mode='contained'
         icon='plus'
-        style={{ margin: 10 }}
         onPress={() => navigation.navigate('AlunoForm')}
       >
         Cadastrar
@@ -47,29 +37,20 @@ export default function AlunoLista({navigation}) {
       <FlatList
         data={alunos}
         renderItem={({ item }) => (
-            <Card 
-                style={{ margin: 10 }} 
-                onPress={() => navigation.navigate('AlunoForm', item.id )}
-            >
-
-                <Card.Content>
-
-                    <Text>ID: {item.id}</Text>
-                    <Text>Nome:{item.nome}</Text>
-                    <Text>CPF: {item.cpf}</Text>
-
-                </Card.Content>
-
-                <Card.Actions>
-
-                    <Button icon='pencil'> </Button>
-                    <Button icon='delete'> </Button>
-
-                </Card.Actions>
-            </Card>
+          <Card style={{ margin: 10 }}>
+            <Card.Content>
+              <Text>ID: {item.id}</Text>
+              <Text>Nome: {item.nome}</Text>
+              <Text>CPF: {item.cpf}</Text>
+            </Card.Content>
+            <Card.Actions>
+              <Button icon='pencil' onPress={() => navigation.navigate('AlunoForm', item)}> </Button>
+              <Button icon='delete' onPress={() => excluirAluno(item.id)}> </Button>
+            </Card.Actions>
+          </Card>
         )}
       />
+
     </View>
   )
 }
-
